163e5c46947247d82cf7b8cf6b86b5bfe4d58371
Bruno Volpe <bruno.volpe2015@gmail.com>
2025-04-26 15:20:18 -0300

    initialize project structure with Spring Boot and Gradle setup

---
+++ src/test/java/com/volpe/encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volpe.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
